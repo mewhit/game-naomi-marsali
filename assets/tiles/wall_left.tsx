30bd9e6cb524f92b5d25cdb378844d55875420d0
Mike <mikewhittom27@gmail.com>
2025-09-23 19:25:46 -0400

    add bath room

--- assets/tiles/wall_left.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="wall_left" tilewidth="64" tileheight="64" tilecount="1" columns="1">
- <image source="wall_left.png" width="64" height="64"/>
-</tileset>
--- assets/tiles/wall_top.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="wall_top" tilewidth="64" tileheight="64" tilecount="1" columns="1">
- <image source="wall_top.png" width="64" height="64"/>
-</tileset>
